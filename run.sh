7fc15bc4ebad8448512b9edb4a5332b193fe5ce7
Abdul Arfan <arfan@srlabs.id>
2020-07-25 21:41:11 +0700

    add ya basic

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ start=`date +%s`
 printf "\nRunning the Scala version : \n"
 scala -nc mergesort.scala
 
+printf "\nRunning the Scala version : \n"
+yabasic hello.bas
+
 end=`date +%s`
 runtime=$((end-start))
 printf "\n"
